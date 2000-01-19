998fc03c555450f1d55fcdbf7d2a80fe44a173c8
gstein <gstein>
2000-01-19 12:31:56 +0000

    update comment header: copyright; add module/tracking info don't display the pseudo-files at the root (play all recursively, etc) add shuffle play  [Bill Tutt] add recognition for other extensions (that WinAmp can handle)  [Bill Tutt] add Content-Length: header; helps when downloading a file, as it provides   a percentage indicator. add ID3 extraction code. (unused so far)

--- edna.py
+++ edna.py
@@ -2,7 +2,7 @@
 #
 # edna.py -- an MP3 server
 #
-# Copyright (C) 1999 Greg Stein. All Rights Reserved.
+# Copyright (C) 1999-2000 Greg Stein. All Rights Reserved.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -18,6 +18,13 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
+#
+# This software is maintained by Greg and is available at:
+#    http://www.lyra.org/greg/edna/
+#
+# Here is the CVS ID for tracking purposes:
+#   $Id: edna.py,v 1.2 2000/01/19 12:31:56 gstein Exp $
+#
 
 import SocketServer
 import BaseHTTPServer
@@ -30,6 +37,8 @@ import urllib
 import StringIO
 import socket
 import re
+import stat
+import whrandom
 
 error = __name__ + '.error'
 
@@ -49,6 +58,7 @@ except ImportError:
 
 class Server(mixin, BaseHTTPServer.HTTPServer):
   def server_bind(self):
+    # we don't need the server name/port, so skip BaseHTTPServer's work
     SocketServer.TCPServer.server_bind(self)
 
   def config(self, config):
@@ -85,7 +95,7 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     if not path:
       subdirs = map(lambda x: (x[1], x[1]+'/'), self.server.dirs)
-      self.display_page('select music', subdirs)
+      self.display_page('select music', subdirs, skiprec=1)
     else:
       for d, name in self.server.dirs:
         if path[0] == name:
@@ -98,14 +108,18 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
       url = '/' + urllib.quote(path[0])
       for i in range(1, len(path)):
         p = path[i]
-        if p == 'all.m3u' or p == 'allrecursive.m3u':
+        if p == 'all.m3u' or p == 'allrecursive.m3u' or \
+           p == 'shuffle.m3u' or p == 'shufflerecursive.m3u':
+          # serve up a pseudo-file
           self.serve_file(p, curdir, url)
           return
+
         pathname = os.path.join(curdir, p)
         base, ext = os.path.splitext(p)
         if string.lower(ext) == '.m3u':
           base, ext = os.path.splitext(base)
-          if string.lower(ext) == '.mp3':
+          if extensions.has_key(string.lower(ext)):
+            # something.mp3.m3u -- one of our psuedo-files
             pathname = os.path.join(curdir, base + ext)
         if not os.path.exists(pathname):
           self.send_error(404)
@@ -120,13 +134,14 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
       songs = []
       playlists = []
       tail = path[-1]
-      taillen = len(path[-1])
+      taillen = len(tail)
       for name in sort_dir(curdir):
         base, ext = os.path.splitext(name)
         ext = string.lower(ext)
-        if ext == '.mp3':
-          # if a song has a prefix that matches the directory, then strip it
-          if base[:taillen] == tail:
+        if extensions.has_key(ext):
+          # if a song has a prefix that matches the directory, and something
+          # exists after that prefix, then strip it
+          if base[:taillen] == tail and len(base) > taillen:
             base = base[taillen:]
 
             # trim a bit of stuff off of the file
@@ -142,7 +157,7 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             subdirs.append(name, name + '/')
       self.display_page('select music', subdirs, songs, playlists)
 
-  def display_page(self, title, subdirs, songs=[], playlists=[]):
+  def display_page(self, title, subdirs, songs=[], playlists=[], skiprec=0):
     self.send_response(200)
     self.send_header("Content-Type", "text/html")
     self.end_headers()
@@ -151,7 +166,7 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                      '<body><h1>%s</h1>\n'
                      % (cgi.escape(title), cgi.escape(title))
                      )
-    self.display_list('Subdirectories', subdirs)
+    self.display_list('Subdirectories', subdirs, skiprec)
     self.display_list('Songs', songs)
     self.display_list('Playlists', playlists)
 
@@ -160,23 +175,34 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     self.wfile.write('</body></html>\n')
 
-  def display_list(self, title, list):
+  def display_list(self, title, list, skipall=0):
     if list:
       self.wfile.write('<p>%s:</p><ul>\n' % title)
       for text, href in list:
         self.wfile.write('<li><a href="%s">%s</a></li>\n' % (urllib.quote(href), cgi.escape(text)))
       self.wfile.write('</ul>\n')
-      if title == 'Songs':
-        self.wfile.write('<p><blockquote><a href="all.m3u">Play all songs</a></blockquote></p>\n')
-      elif title == 'Subdirectories' : 
-        self.wfile.write('<p><blockquote><a href="allrecursive.m3u">Play all songs (recursively)</a></blockquote></p>\n')
+      if not skipall:
+        if title == 'Songs':
+          self.wfile.write('<p><blockquote>'
+                           '<a href="all.m3u">Play all songs</a>'
+                           '<br>'
+                           '<a href="shuffle.m3u">Shuffle all songs</a>'
+                           '</blockquote></p>\n')
+        elif title == 'Subdirectories' : 
+          self.wfile.write('<p><blockquote>'
+                           '<a href="allrecursive.m3u">Play all songs (recursively)</a>'
 
-  def make_list(self, fullpath, url, recursive, songs=None):
+                           '<br>'
+                           '<a href="shufflerecursive.m3u">Shuffle all songs (recursively)</a>'
+                           '</blockquote></p>\n')
+
+  def make_list(self, fullpath, url, recursive, shuffle, songs=None):
     # This routine takes a string for 'fullpath' and 'url', a list for
-    # 'songs' and a boolean for 'recursive'. If recursive is false make_list
-    # will return a list of every file ending in '.mp3' in fullpath. If
-    # recursive is true make_list will return a list of every file ending
-    # in '.mp3' in fullpath and in every directory beneath fullpath.
+    # 'songs' and a boolean for 'recursive' and 'shuffle'. If recursive is
+    # false make_list will return a list of every file ending in '.mp3' in
+    # fullpath. If recursive is true make_list will return a list of every
+    # file ending in '.mp3' in fullpath and in every directory beneath
+    # fullpath.
     #
     # WARNING: There is no checking for the recursive directory structures
     # which are possible in most Unixes using ln -s etc...  If you have
@@ -189,7 +215,7 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
       songs = []
 
     for name in sort_dir(fullpath):
-      if name[-4:] == '.mp3':
+      if extensions.has_key(name[-4:]):
         # add the song's URL to the list we're building
         songs.append(self.build_url(url, name) + '\n')
 
@@ -197,38 +223,52 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
       if recursive and os.path.isdir(fullpath + '/' + name):
         songs = self.make_list(fullpath + '/' + name,
                                url + '/' + urllib.quote(name),
-                               recursive,
+                               recursive, 0,	# don't shuffle subdir results
                                songs)
+
+    # The user asked us to mix up the results.
+    if shuffle:
+      count = len(songs)
+      for i in xrange(count):
+        j = int(whrandom.random() * count)
+        songs[i], songs[j] = songs[j], songs[i]
 
     return songs
 
   def serve_file(self, name, fullpath, url):
     base, ext = os.path.splitext(name)
     ext = string.lower(ext)
-    if ext == '.mp3':
-      type = 'video/mpeg'
+    if extensions.has_key(ext):
+      type = extensions[ext]
       f = open(fullpath, 'rb')
+      clen = os.fstat(f.fileno())[stat.ST_SIZE]
     elif ext != '.m3u':
       self.send_error(404)
       return
     else:
       type = 'audio/x-mpegurl'
-      if name == 'all.m3u' or name == 'allrecursive.m3u':
-        recursive = name == 'allrecursive.m3u'
+      if name == 'all.m3u' or name == 'allrecursive.m3u' or \
+         name == 'shuffle.m3u' or name == 'shufflerecursive.m3u':
+        recursive = name == 'allrecursive.m3u' or name == 'shufflerecursive.m3u'
+        shuffle = name == 'shuffle.m3u' or name == 'shufflerecursive.m3u'
 
         # generate the list of URLs to the songs
-        songs = self.make_list(fullpath, url, recursive)
+        songs = self.make_list(fullpath, url, recursive, shuffle)
 
         f = StringIO.StringIO(string.join(songs, ''))
+        clen = len(f.getvalue())
       else:
         base, ext = os.path.splitext(base)
-        if ext == '.mp3':
-          f = StringIO.StringIO(self.build_url(url, base) + '.mp3\n')
+        if extensions.has_key(string.lower(ext)):
+          f = StringIO.StringIO(self.build_url(url, base) + ext + '\n')
+          clen = len(f.getvalue())
         else:
           f = open(fullpath)
+          clen = os.fstat(f.fileno())[stat.ST_SIZE]
 
     self.send_response(200)
     self.send_header("Content-Type", type)
+    self.send_header("Content-Length", clen)
     self.end_headers()
 
     while 1:
@@ -298,10 +338,78 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
       pass
 
 
+_genres = [
+  "Blues", "Classic Rock", "Country", "Dance", "Disco", "Funk", "Grunge",
+  "Hip-Hop", "Jazz", "Metal", "New Age", "Oldies", "Other", "Pop", "R&B",
+  "Rap", "Reggae", "Rock", "Techno", "Industrial", "Alternative", "Ska",
+  "Death Metal", "Pranks", "Soundtrack", "Euro-Techno", "Ambient", "Trip-Hop",
+  "Vocal", "Jazz+Funk", "Fusion", "Trance", "Classical", "Instrumental",
+  "Acid", "House", "Game", "Sound Clip", "Gospel", "Noise", "AlternRock",
+  "Bass", "Soul", "Punk", "Space", "Meditative", "Instrumental Pop",
+  "Instrumental Rock", "Ethnic", "Gothic", "Darkwave", "Techno-industrial",
+  "Electronic", "Pop-Folk", "Eurodance", "Dream", "Southern Rock", "Comedy",
+  "Cult", "Gangsta", "Top 40", "Christian Rap", "Pop/Funk", "Jungle",
+  "Native American", "Cabaret", "New Wave", "Psychadelic", "Rave",
+  "Showtunes", "Trailer", "Lo-Fi", "Tribal", "Acid Punk", "Acid Jazz",
+  "Polka", "Retro", "Musical", "Rock & Roll", "Hard Rock", "Folk",
+  "Folk/Rock", "National Folk", "Swing", "Fast-Fusion", "Bebob", "Latin",
+  "Revival", "Celtic", "Bluegrass", "Avantegarde", "Gothic Rock",
+  "Progressive Rock", "Psychedelic Rock", "Symphonic Rock", "Slow Rock",
+  "Big Band", "Chorus", "Easy Listening", "Acoustic", "Humour", "Speech",
+  "Chanson", "Opera", "Chamber Music", "Sonata", "Symphony", "Booty Bass",
+  "Primus", "Porn Groove", "Satire", "Slow Jam", "Club", "Tango", "Samba",
+  "Folklore", "Ballad", "Power Ballad", "Rythmic Soul", "Freestyle", "Duet",
+  "Punk Rock", "Drum Solo", "A capella", "Euro-House", "Dance Hall", "Goa",
+  "Drum & Bass", "Club House", "Hardcore", "Terror", "Indie", "BritPop",
+  "NegerPunk", "Polsk Punk", "Beat", "Christian Gangsta", "Heavy Metal",
+  "Black Metal", "Crossover", "Contemporary C", "Christian Rock", "Merengue",
+  "Salsa", "Thrash Metal", "Anime", "JPop", "SynthPop",
+  ]
+
+class ID3:
+  def __init__(self, file):
+    self.valid = 0
+    file.seek(-128, 2)	# 128 bytes before the end of the file
+    if file.read(3) == 'TAG':
+      self.title = string.strip(file.read(30))
+      self.artist = string.strip(file.read(30))
+      self.album = string.strip(file.read(30))
+      self.year = string.strip(file.read(4))
+      self.comment = string.strip(file.read(30))	### 29 or 30??
+
+      genre = ord(file.read(1))
+      if genre < len(_genres):
+        self.genre = _genres[genre]
+      else:
+        self.genre = 'unknown'
+
+      self.valid = 1
+
+    file.seek(0, 0)
+
 def sort_dir(d):
   l = os.listdir(d)
   l.sort()
   return l
+
+# Extensions that WinAMP can handle: (and their MIME type if applicable)
+extensions = { 
+  '.mp3' : 'video/mpeg',
+  '.mid' : 'audio/mid',
+  '.mp2' : 'video/mpeg',
+#  '.cda',
+  '.it'  : 'audio/mid',
+  '.xm'  : 'audio/mid',
+  '.s3m' : 'audio/mid',
+  '.stm' : 'audio/mid',
+  '.mod' : 'audio/mid',
+  '.dsm' : 'audio/mid',
+  '.far' : 'audio/mid',
+  '.ult' : 'audio/mid',
+  '.mtm' : 'audio/mid',
+  '.669' : 'audio/mid',
+  '.asx' : 'video/x-ms-asf',
+  }
 
 if __name__ == '__main__':
   if len(sys.argv) > 2:
